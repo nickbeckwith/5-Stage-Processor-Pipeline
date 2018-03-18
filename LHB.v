ded5dc3c9e09fc21a160e16db4a701b9cf52e576
Arnav Sharma <sharma55@wisc.edu>
2018-03-18 14:55:50 -0500

    Update LHB.v

--- LHB.v
+++ LHB.v
@@ -1,4 +1,4 @@
-module LHB(input A[15:0], output [15:0] Upper_byte); 
+module LHB(input[15:0] A, output [15:0] Upper_byte); 
 	  
 	assign Upper_byte = {A[15],A[15],A[15],A[15],A[15],A[15],A[15],A[15],A[15:8]};
 endmodule 
