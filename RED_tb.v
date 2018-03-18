d78c07578c5e1e5aa9c9de36f92fd8a29293b5ce
Justin <jsnydes18@gmail.com>
2018-03-18 15:52:44 -0500

    alu_compute, ready for testing

--- RED_tb.v
+++ RED_tb.v
@@ -1,6 +1,6 @@
 module RED_tb(); 
 	reg[15:0] A,B; 
-	wire [6:0] R; 
+	wire [15:0] R; 
 	
 	RED iDUT(.A(A),.B(B),.R(R));
 	
@@ -10,7 +10,7 @@ module RED_tb();
 		A = 16'd1; 
 		B = 16'd2; 
 		#10; 
-		if(R != 7'd3)begin 
+		if(R != 16'd3)begin 
 			$display("error");
 		end
 	end
--- alu_compute.v
+++ alu_compute.v
@@ -1,7 +1,8 @@
-module alu_compute(InputA, InputB, Opcode, OutputA, OutputB);
+module alu_compute(InputA, InputB, Opcode, OutputA, OutputB, Flag);
 	input [15:0] InputA, InputB;
 	input [3:0] Opcode;
 	output [15:0] OutputA, OutputB;
+	output [2:0] Flag;
 	
 	wire [15:0] addsub_o;
 	wire [2:0] addsub_f;
@@ -20,16 +21,29 @@ module alu_compute(InputA, InputB, Opcode, OutputA, OutputB);
 	wire [15:0] paddsb_o;
 	paddsb PADDSB (InputA, InputB, paddsb_o);
 
-	wire [15:0] mem_addr;
-	add_16b MEMADD (.a(InputA), .b(InputB), .cin(1'b0), .s(lw_addr));
+	wire [15:0] mem_addr, rs_even, imm_shift;
+	assign rs_even = InputA & 16'b1111111111111110;
+	assign imm_shift = InputB << 1;
+	add_16b MEMADD (.a(rs_even), .b(imm_shift), .cin(1'b0), .s(mem_addr));
+	assign OutputA = mem_addr;
 
 	wire[15:0] MA_out;
 	mux8_1_16b MA (.d0(addsub_o), .d1(addsub_o), .d2(red_o), .d3(xor_o), .d4(shift_o), .d5(shift_o), .d6(shift_o), .d7(paddsb_o), .b(MA_out), .s(Opcode[2:0]));
-
-	wire[15:0] LLB_LHB_o, MB_out;
-
-	mux2_1_16b MB (.d0(MA_out), .d1(LLB_LHB_o), .b(MB_out), .s(Opcode[3]));
 	
-	assign OutputA = mem_addr;
+	wire [15:0] LLB, LHB, LXX_o;
+	assign LLB = (InputA & 16'b1111111100000000) | InputB;
+	assign LHB = (InputA & 16'b0000000011111111) | (InputB << 8);
+	assign LXX_o = Opcode[0] ? LLB : LHB;
+
+	wire [15:0] MB_out;
+	mux2_1_16b MB (.d0(MA_out), .d1(LXX_o), .b(MB_out), .s(Opcode[3]));
+	
 	assign OutputB = MB_out;
+
+	//0 = Z
+	assign Flag[0] = MA_out == 16'b0000000000000000 ? 1'b1 : 1'b0;
+	//1 = V
+	assign Flag[1] = addsub_f[1];
+	//2 = N 
+	assign Flag[2] = addsub_f[2];
 endmodule
