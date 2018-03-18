d5f7924f639bb1900fd18b619dc4274ca0849b2d
Justin <jsnydes18@gmail.com>
2018-03-18 16:48:04 -0500

    Removing unnecessary files

--- LHB.v
+++
@@ -1,4 +0,0 @@
-module LHB(input[15:0] A, output [15:0] Upper_byte); 
-	  
-	assign Upper_byte = {A[15],A[15],A[15],A[15],A[15],A[15],A[15],A[15],A[15:8]};
-endmodule 
--- LLB.v
+++
@@ -1,4 +0,0 @@
-module LLB(input [15:0]A, output [15:0] Lower_byte); 
-  
-	assign Lower_byte = {A[7],A[7],A[7],A[7],A[7],A[7],A[7],A[7],A[7:0]};
-endmodule 
