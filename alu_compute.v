81d9a2e5fbb8ffeece8bdebc075bfbdb390dbfdb
Justin <jsnydes18@gmail.com>
2018-03-18 14:54:35 -0500

    updating to pull LLB and LHB modules

--- alu_compute.v
+++ alu_compute.v
@@ -8,7 +8,7 @@ module alu_compute(InputA, InputB, Opcode, OutputA, OutputB);
 	alu_adder ADDSUB (Opcode[0], InputA, InputB, addsub_o, addsub_f[2], addsub_f[1], addsub_f[0]);
 
 	wire [15:0] red_o;
-	red RED (InputA, InputB, red_o);
+	RED RED_mod (InputA, InputB, red_o);
 
 	wire [15:0] xor_o;
 	xor_16b XOR (InputA, InputB, xor_o);
@@ -26,6 +26,10 @@ module alu_compute(InputA, InputB, Opcode, OutputA, OutputB);
 	wire[15:0] MA_out;
 	mux8_1_16b MA (.d0(addsub_o), .d1(addsub_o), .d2(red_o), .d3(xor_o), .d4(shift_o), .d5(shift_o), .d6(shift_o), .d7(paddsb_o), .b(MA_out), .s(Opcode[2:0]));
 
+	wire[15:0] LLB_LHB_o, MB_out;
+
+	mux2_1_16b MB (.d0(MA_out), .d1(LLB_LHB_o), .b(MB_out), .s(Opcode[3]));
+	
 	assign OutputA = mem_addr;
-	assign OutputB = MA_out;
+	assign OutputB = MB_out;
 endmodule
