0046217fd78beb333c14abba55705ddbf3bd26a2
Justin <jsnydes18@gmail.com>
2018-03-18 21:35:56 -0500

    Pushing test bench stuff

--- dmemory.v
+++ dmemory.v
@@ -52,7 +52,7 @@ module dmemory (data_out, data_in, addr, enable, wr, clk, rst);
       if (rst) begin
          //load loadfile_all.img
          if (!loaded) begin
-            $readmemh("loadfile_all.img", mem);
+            $readmemh("dmem_init.img", mem);
             loaded = 1;
          end
           
