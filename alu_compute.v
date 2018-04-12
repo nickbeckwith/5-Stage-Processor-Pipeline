2a941773c702ce126630d96666586437fcffda40
Nick Beckwith <nbeckwith2@wisc.edu>
2018-04-12 00:22:42 -0500

    Fixes don't care mistake and turns ALU output into case

--- alu_compute.v
+++ alu_compute.v
@@ -28,21 +28,34 @@ module alu_compute(InputA, InputB, Offset, Opcode, OutputA, OutputB, Flag);
 	assign OutputA = mem_addr;
 
 	wire [15:0] MA_out;
+	always begin
+		case (Opcode[2:0])
+			0 : MA_out = addsub_o;
+			1 : MA_out = addsub_o;
+			2 : MA_out = red_o;
+			3 : MA_out = xor_o;
+			4 : MA_out = shift_o;
+			5 : MA_out = shift_o;
+			6 : MA_out = shift_o;
+			7 : MA_out = paddsb_o;
+			default: MA_out = 16'hxxxx;		// this should not happen
+		endcase
+	end
 	mux8_1_16b MA (.d0(addsub_o), .d1(addsub_o), .d2(red_o), .d3(xor_o), .d4(shift_o),
 										.d5(shift_o), .d6(shift_o), .d7(paddsb_o), .b(MA_out),
 										.s(Opcode[2:0]));
 
 	wire [15:0] MB_out, MC_out;
 	wire lw_sw;
-	assign lw_sw = Opcode == 4'b100? ? 1'b1 : 1'b0;
-	mux2_1_16b MB (.d0(MA_out), .d1(InputB), .b(MB_out), .s(lw_sw);
+	assign lw_sw = Opcode == {3'b100, Opcode[0]} ? 1'b1 : 1'b0;
+	mux2_1_16b MB (.d0(MA_out), .d1(InputB), .b(MB_out), .s(lw_sw));
 
 	wire [15:0] LLB, LHB, LXX_o;
 	assign LLB = (InputA & 16'b1111111100000000) | Offset;
 	assign LHB = (InputA & 16'b0000000011111111) | (Offset << 8);
 	assign LXX_o = Opcode[0] ? LLB : LHB;
 
-	assign MC_out = Opcode == 4'b101? ? LXX_o : MB_out;
+	assign MC_out = Opcode == {3'b101, Opcode[0]} ? LXX_o : MB_out;
 	assign OutputB = MC_out;
 
 	//0 = Z
