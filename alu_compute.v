9c7ba6f5f5b4e2997f2582597df6faf4a22a99f6
Nick Beckwith <nbeckwith2@wisc.edu>
2018-04-12 00:47:11 -0500

    Improves readability of alu_compute

--- alu_compute.v
+++ alu_compute.v
@@ -27,7 +27,14 @@ module alu_compute(InputA, InputB, Offset, Opcode, OutputA, OutputB, Flag);
 	add_16b MEMADD (.a(rs_even), .b(imm_shift), .cin(1'b0), .s(mem_addr), .cout());
 	assign OutputA = mem_addr;
 
-	wire [15:0] MA_out;
+	wire [15:0] LLB, LHB, LXX_o;
+	assign LLB = (InputA & 16'b1111111100000000) | Offset;
+	assign LHB = (InputA & 16'b0000000011111111) | (Offset << 8);
+	assign LXX_o = Opcode[0] ? LLB : LHB;
+
+	// DETERMINES WHICH OPERATION PASSES AS AN OUTPUT
+	//////////////////////////////////////////////////////////////
+	reg [15:0] MA_out;
 	always begin
 		case (Opcode[2:0])
 			0 : MA_out = addsub_o;
@@ -41,25 +48,20 @@ module alu_compute(InputA, InputB, Offset, Opcode, OutputA, OutputB, Flag);
 			default: MA_out = 16'hxxxx;		// this should not happen
 		endcase
 	end
-	mux8_1_16b MA (.d0(addsub_o), .d1(addsub_o), .d2(red_o), .d3(xor_o), .d4(shift_o),
-										.d5(shift_o), .d6(shift_o), .d7(paddsb_o), .b(MA_out),
-										.s(Opcode[2:0]));
 
 	wire [15:0] MB_out, MC_out;
-	wire lw_sw;
-	assign lw_sw = Opcode == {3'b100, Opcode[0]} ? 1'b1 : 1'b0;
-	mux2_1_16b MB (.d0(MA_out), .d1(InputB), .b(MB_out), .s(lw_sw));
+	// MB_out gets InputB if LW or SW instruction
+	assign MB_out = ((Opcode == 4'b1000) | (Opcode == 4'b1001)) ? InputB : MA_out;
 
-	wire [15:0] LLB, LHB, LXX_o;
-	assign LLB = (InputA & 16'b1111111100000000) | Offset;
-	assign LHB = (InputA & 16'b0000000011111111) | (Offset << 8);
-	assign LXX_o = Opcode[0] ? LLB : LHB;
-
-	assign MC_out = Opcode == {3'b101, Opcode[0]} ? LXX_o : MB_out;
+	// MC_out gets LXX_o if the opcode is an LLB or LHB instruction
+	assign MC_out = (Opcode == 4'b1010) | (Opcode == 4'b1011) ? LXX_o : MB_out;
 	assign OutputB = MC_out;
 
+	/////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////
+
 	//0 = Z
-	assign Flag[0] = MB_out == 16'b0000000000000000 ? 1'b1 : 1'b0;
+	assign Flag[0] = MB_out == 16'b0 ? 1'b1 : 1'b0;
 	//1 = V
 	assign Flag[1] = addsub_f[1] & ~(Opcode[3] | Opcode[2] | Opcode[1]);
 	//2 = N
