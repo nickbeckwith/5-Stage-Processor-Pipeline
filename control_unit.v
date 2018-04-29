190dc60a69186819809701fdf1da4193653f4801
Nick Beckwith <nbeckwith2@wisc.edu>
2018-04-29 18:14:17 -0500

    Improves readability of control_unit

--- control_unit.v
+++ control_unit.v
@@ -1,4 +1,4 @@
-module control_unit(opcode, reg_wren, mem_to_reg, mem_wr, alu_src, reg_dst,
+module control_unit(opcode, reg_wren, mem_to_reg, mem_wr, alu_src, dst_reg_sel,
          branch);
    input [3:0]
       opcode;
@@ -7,6 +7,6 @@ module control_unit(opcode, reg_wren, mem_to_reg, mem_wr, alu_src, reg_dst,
       mem_to_reg,       // Enables reading from memory and writes to reg
       mem_wr,           // Enables writing to memory
       alu_src,          // 0 is reg, 1 is imm value. Reg/IMm selector
-      reg_dst,          // 1 is ALU, 0 is memory. ALU/mem out selector
+      dst_reg_sel,      // write to register in rt or rd. rt is 0
       branch;           // Is this a branch operation?
 endmodule
