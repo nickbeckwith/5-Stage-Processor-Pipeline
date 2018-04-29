132a68bd8e76eb8c7cf893ffc9303382b2345c5e
Nick Beckwith <nbeckwith2@wisc.edu>
2018-04-29 13:31:29 -0500

    Put flag_reg inside ALU_compute

--- alu_compute.v
+++ alu_compute.v
@@ -1,65 +1,86 @@
 `include "alu_compute.vh"
 
-module alu_compute(InputA, InputB, Offset, Shift_Imm, Opcode, OutputA, OutputB, Flag);
-	input [15:0] InputA, InputB, Offset;
-	input [3:0] Opcode, Shift_Imm;
-	output [15:0] OutputA, OutputB;
-	output [2:0] Flag;
+module alu_compute(input_A, input_B, offset, shift_imm, opcode, output_A, output_B, flag);
+	input [15:0] input_A, input_B, offset;
+	input [3:0] opcode, shift_imm;
+	output [15:0] output_A, output_B;
+	output [2:0] flag;
 
 	wire [15:0] addsub_o;
 	wire [2:0] addsub_f;
-	alu_adder ADDSUB (Opcode[0], InputA, InputB, addsub_o, addsub_f[2], addsub_f[1], addsub_f[0]);
+	alu_adder ADDSUB(
+		.mode(opcode[0]),
+		.A(input_A),
+		.B(input_B),
+		.S(addsub_o),
+		.N(addsub_f[2]),
+		.V(addsub_f[1]),
+		.Z(addsub_f[0]));
 
 	wire [15:0] red_o;
-	RED RED_mod (InputA, InputB, red_o);
+	RED RED_mod (input_A, input_B, red_o);
 
 	wire [15:0] xor_o;
-	assign xor_o = InputA ^ InputB;
+	assign xor_o = input_A ^ input_B;
 
 	wire [15:0] shift_o;
 	wire shift_f;
-	shifter SHIFT (.Shift_Out(shift_o), .Zero(shift_f), .Shift_In(InputA), .Shift_Val(Shift_Imm), .Mode(Opcode[1:0]));
+	shifter SHIFT(
+		.Shift_Out(shift_o),
+		.Zero(shift_f),
+		.Shift_In(input_A),
+		.Shift_Val(shift_imm),
+		.Mode(opcode[1:0]));
 
 	wire [15:0] paddsb_o;
-	paddsb PADDSB (InputA, InputB, paddsb_o);
+	paddsb PADDSB(input_A, input_B, paddsb_o);
 
 	wire [15:0] rs_even, imm_shift;
-	assign rs_even = InputA & 16'b1111111111111110;
-	assign imm_shift = Offset << 1;
-	add_16b MEMADD (.a(rs_even), .b(imm_shift), .cin(1'b0), .s(OutputA), .cout());
+	assign rs_even = input_A & 16'b1111111111111110;
+	assign imm_shift = offset << 1;
+	add_16b MEMADD(.a(rs_even), .b(imm_shift), .cin(1'b0), .s(output_A), .cout());
 
 	wire [15:0] LLB, LHB;
-	assign LLB = {InputA[15:8], Offset[7:0]};
-	assign LHB = {Offset[7:0], InputA[7:0]};
+	assign LLB = {input_A[15:8], offset[7:0]};
+	assign LHB = {offset[7:0], input_A[7:0]};
 
 	// DETERMINES WHICH OPERATION PASSES AS AN OUTPUT
 	//////////////////////////////////////////////////////////////
-	reg [15:0] OutputB_im;
+	reg [15:0] output_B_im;
 	always @* begin
-		casez (Opcode)
-			`SW 			: OutputB_im = InputB;
-			`LHB 			: OutputB_im = LHB;
-			`LLB 			: OutputB_im = LLB;
-			`ADD 			: OutputB_im = addsub_o;
-			`SUB 			: OutputB_im = addsub_o;
-			`RED 			: OutputB_im = red_o;
-			`XOR 			: OutputB_im = xor_o;
-			`SLL 			: OutputB_im = shift_o;
-			`SRA 			: OutputB_im = shift_o;
-			`ROR 			: OutputB_im = shift_o;
-			`PADDSB 	: OutputB_im = paddsb_o;
-			default: OutputB_im = 16'h0000;
+		casez (opcode)
+			`SW 			: output_B_im = input_B;
+			`LHB 			: output_B_im = LHB;
+			`LLB 			: output_B_im = LLB;
+			`ADD 			: output_B_im = addsub_o;
+			`SUB 			: output_B_im = addsub_o;
+			`RED 			: output_B_im = red_o;
+			`XOR 			: output_B_im = xor_o;
+			`SLL 			: output_B_im = shift_o;
+			`SRA 			: output_B_im = shift_o;
+			`ROR 			: output_B_im = shift_o;
+			`PADDSB 	: output_B_im = paddsb_o;
+			default: output_B_im = 16'h0000;
 		endcase
 	end
-	assign OutputB = OutputB_im;
+	assign output_B = output_B_im;
 	/////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////
-
+	wire [2:0] flag_imm;
 	// flags. Flag register is write restricted depending on opcode
 	//0 = Z
-	assign Flag[0] = OutputB == 16'h0000;
+	assign flag_imm[0] = output_B == 16'h0000;
 	//1 = V
-	assign Flag[1] = addsub_f[1];
+	assign flag_imm[1] = addsub_f[1];
 	//2 = N
-	assign Flag[2] = addsub_f[2];
+	assign flag_imm[2] = addsub_f[2];
+
+	// need to determine when to write each bit to reg
+	wire [2:0] wrt_en;
+	// if it's non arithmetic op, RED or paddsb don't write to zero reg
+	assign wrt_en[0] = ~(opcode[3] | (opcode == `RED) | (opcode == `PADDSB));
+	assign wrt_en[2:1] = (opcode == `ADD) | (opcode == `SUB);
+
+	flag_reg flag(.clk(clk), .rst(rst), .d(flag_imm), .wrt_en(wrt_en), .q(flag));
+
 endmodule
--- flag_reg.v
+++ flag_reg.v
@@ -1,13 +1,19 @@
-`include "BitCell.v"
+`include "dff.v"
 
-module flag_reg( input clk,  input rst, input [2:0] D, input[2:0] WriteReg, input ReadEnable1, input ReadEnable2, output [2:0] Bitline1, output [2:0] Bitline2);
-	wire[2:0] bit_out;
+module flag_reg(clk, rst, d, wrt_en, q);
+	input
+		clk,
+		rst;
+	input [2:0]
+		wrt_en,
+		d;
+	output [2:0]
+		q;
+	wire[2:0] dff_out;
 
-	BitCell cell_0(.clk(clk),.rst(rst),.D(D[0]), .WriteEnable(WriteReg[0]),.ReadEnable1(ReadEnable1),.ReadEnable2(ReadEnable2),.Bitline1(bit_out[0]),.Bitline2());
-	BitCell cell_1(.clk(clk),.rst(rst),.D(D[1]), .WriteEnable(WriteReg[1]),.ReadEnable1(ReadEnable1),.ReadEnable2(ReadEnable2),.Bitline1(bit_out[1]),.Bitline2());
-	BitCell cell_2(.clk(clk),.rst(rst),.D(D[2]), .WriteEnable(WriteReg[2]),.ReadEnable1(ReadEnable1),.ReadEnable2(ReadEnable2),.Bitline1(bit_out[2]),.Bitline2());
-	
-	assign Bitline1[0] = WriteReg[0] ? D[0] : bit_out[0]; 
-	assign Bitline1[1] = WriteReg[1] ? D[1] : bit_out[1]; 
-	assign Bitline1[2] = WriteReg[2] ? D[2] : bit_out[2]; 
+	dff ff[2:0](.q(dff_out), .d(d), .wen(wrt_en), .clk(clk), .rst(rst));
+
+	// implement forwarding.
+	assign q = (wrt_en & d) | (~wrt_en & dff_out);
+
 endmodule
