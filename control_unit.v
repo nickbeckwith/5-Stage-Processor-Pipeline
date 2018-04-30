3db4b886999fdb54c0f0b59a68dfe6eff1a2f2ab
Nick Beckwith <nbeckwith2@wisc.edu>
2018-04-29 22:11:46 -0500

    Make control_unit tad quicker

--- control_unit.v
+++ control_unit.v
@@ -1,3 +1,4 @@
+`include "opcodes.vh"
 module control_unit(opcode, reg_wren, mem_to_reg, mem_wr, alu_src, dst_reg_sel,
          branch);
    input [3:0]
@@ -10,22 +11,20 @@ module control_unit(opcode, reg_wren, mem_to_reg, mem_wr, alu_src, dst_reg_sel,
       dst_reg_sel,      // write to register in rt or rd. rt is 0
       branch;           // Is this a branch operation?
 
-      assign branch = (opcode ==`B | opcode ==`BR) ? 1 : 0;
+      assign branch = (opcode ==`B | opcode ==`BR);
 
-      assign mem_to_reg = (opcode ==`LW) ? 1 : 0;
+      assign mem_to_reg = opcode ==`LW;
 
-      assign mem_wr = (opcode == `SW) ? 1 : 0;
+      assign mem_wr = opcode == `SW;
 
-      assign alu_src = ((opcode == `LW) | (opcode == `SW) |
+      assign alu_src = (opcode == `LW) | (opcode == `SW) |
                         (opcode == `ROR) | (opcode == `SLL) |
-                        (opcode == `SRA)) ? 1 : 0 ;
+                        (opcode == `SRA);
 
+      assign dst_reg_sel = ~(opcode == `LW);
 
-      assign dst_reg_sel = ((opcode == `LW)) ? 0 : 1;
+      // enabled on all arithmetic and some memory ops
+      assign reg_wren = (~opcode[3]) | (opcode == `LW) | (opcode == `LHB) |
+                        (opcode == `LLB) | (opcode == `PCS);
 
-      assign reg_wren = (opcode == `LLB | opcode == `LHB | opcode == `LW |
-                         opcode == `SLL | opcode == `SRA | opcode == `ROR |
-                         opcode == `ADD | opcode == `SUB | opcode == `PCS |
-                         opcode == `PADDSB | opcode == `RED | opcode == `XOR) ?
-                         1 : 0;
 endmodule
