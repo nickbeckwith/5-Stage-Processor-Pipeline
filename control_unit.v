b1f9fad249825c455680519b9ca7a3d90dbb55bc
Arnav <sharma55@wisc.edu>
2018-04-29 20:02:34 -0500

    control unit implementation

--- control_unit.v
+++ control_unit.v
@@ -9,4 +9,24 @@ module control_unit(opcode, reg_wren, mem_to_reg, mem_wr, alu_src, dst_reg_sel,
       alu_src,          // 0 is reg, 1 is imm value. Reg/IMm selector
       dst_reg_sel,      // write to register in rt or rd. rt is 0
       branch;           // Is this a branch operation?
+
+      assign branch = (opcode ==`B | opcode ==`BR) ? 1 : 0;
+      assign mem_to_reg = (opcode ==`LW) ? 1 : 0;
+
+      assign mem_wr = (opcode == `SW) ? 1 : 0;
+
+      assign alu_src = ((opcode == `LW) | (opcode == `SW) |
+                        (opcode == `ROR) | (opcode == `SLL) |
+                        (opcode == `SRA)) ? 1 : 0 ;
+
+      assign reg_wren = (opcode == `SW | opcode == `BR | opcode == `B |
+                         opcode == `HLT) ? 0 : 1;
+
+      assign dst_reg_sel = ((opcode == `LW)) ? 0 : 1;
+
+      assign reg_wren = (opcode == `LLB | opcode == `LHB | opcode == `LW |
+                         opcode == `SLL | opcode == `SRA | opcode == `ROR |
+                         opcode == `ADD | opcode == `SUB | opcode == `PCS |
+                         opcode == `PADDSB | opcode == `RED | opcode == `XOR) ?
+                         1 : 0;
 endmodule
