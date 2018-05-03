30959681115765fdda486d11024ca0bb5e849f1d
Nick Beckwith <nbeckwith2@wisc.edu>
2018-05-03 15:11:59 -0500

    Use verilog constructs to simplify adder

--- add_1b.v
+++
@@ -1,8 +0,0 @@
-module add_1b(a, b, cin, s, p, g);
-	input a, b, cin;
-	output s, p, g;
-
-	assign s = (a ^ b) ^ cin;
-	assign p = a ^ b;
-	assign g = a & b;
-endmodule
--- alu_adder.v
+++ alu_adder.v
@@ -1,19 +1,14 @@
-module alu_adder(mode, A, B, S, N, V, Z);
+module alu_adder(mode, A, B, S, N, V);
 	input mode;
 	input [15:0] A, B;
 	output [15:0] S;
 	output N, V, Z;
 
-	wire [15:0] mode_ext, bin;
-	assign mode_ext = {16{mode}};
-	assign bin = B ^ mode_ext;
-
 	wire [15:0] sw;
 	wire cout;
-	add_16b ADD (A, bin, mode, sw, cout);
+	assign {cout, sw} = mode ? A - B : A + B;
 
 	assign N = S[15];
-	assign Z = S == 16'b0 ? 1'b1 : 1'b0;
 	assign V = A[15] & B[15] & ~(mode) & ~(sw[15]) | ~(A[15]) & ~(B[15]) & ~(mode) & sw[15] | A[15] & ~(B[15]) & mode & ~(sw[15]) | ~(A[15]) & B[15] & mode & sw[15];
 
 	wire [15:0] saturate;
